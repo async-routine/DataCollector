d389e557173ef1f148f6039dbf3d896f926f360c
anish <anishvisaria98@gmail.com>
2018-09-04 22:21:38 -0400

    firebase integration

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Button } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
 import t from 'tcomb-form-native'; // 0.6.9
-
 import { WeatherWidget } from 'react-native-weather';
+import firebase from 'firebase';
+import 'firebase/firestore';
 
-// import { Auth } from 'aws-amplify';
+firebase.initializeApp({
+  apiKey: "AIzaSyAa8LL5i7gX2OtHuHfxc_ZJe7PMXXmi8EQ",
+  authDomain: "datacollector-18b4f.firebaseapp.com",
+  databaseURL: "https://datacollector-18b4f.firebaseio.com",
+  projectId: "datacollector-18b4f",
+  storageBucket: "datacollector-18b4f.appspot.com",
+  messagingSenderId: "622640505219"
+});
+var db = firebase.firestore();
+const settings = {
+  timestampsInSnapshots: true
+}
+db.settings(settings);
 
 const Form = t.form.Form;
 
@@ -35,8 +47,8 @@ const options = {
 
 export default class App extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
     this.state = {
       value: null
     }
@@ -45,10 +57,30 @@ export default class App extends Component {
   handleSubmit = () => {
     const value = this._form.getValue(); // use that ref to get the form value
     console.log('value: ', value);
+    db.collection("users").add({
+      name: value.name,
+      email: value.email,
+      major: value.major,
+      subscribe: value.subscribe
+    })
+      .then(function (docRef) {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
     this.setState({ value: null })
   }
 
   render() {
+    const styles = StyleSheet.create({
+      container: {
+        justifyContent: 'center',
+        marginTop: 50,
+        padding: 20,
+        backgroundColor: '#ffffff',
+      },
+    });
     return (
       <View style={styles.container}>
         <Form
@@ -62,12 +94,11 @@ export default class App extends Component {
           title="Sign Up!"
           onPress={this.handleSubmit}
         />
-        
 
         <WeatherWidget
-        api={"4dc6527f121fa835f86ad34a1ae3eb7a"}
-        lat={"33.748997"}
-        lng={"-84.387985"}
+          api={"4dc6527f121fa835f86ad34a1ae3eb7a"}
+          lat={"33.748997"}
+          lng={"-84.387985"}
         />
       </View>
 
@@ -75,12 +106,3 @@ export default class App extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    marginTop: 50,
-    padding: 20,
-    backgroundColor: '#ffffff',
-  },
-});
